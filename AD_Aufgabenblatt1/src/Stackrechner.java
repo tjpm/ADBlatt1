3a3aa46881901b5278d570f2cd9a608bf7d1f994
Slave <Slave@Bean.localdomain>
2018-10-14 18:57:43 +0200

    erster ansatz stackrechner, noch mit fehlern

--- AD_Aufgabenblatt1/src/Stackrechner.java
+++ AD_Aufgabenblatt1/src/Stackrechner.java
@@ -11,14 +11,52 @@ public class Stackrechner
 		Stack<String> operator = new Stack<String>();
 		Stack<Integer> operand = new Stack<Integer>();
 		String s = "";
-		Integer igd = new Integer(21);
-		s = JOptionPane.showInputDialog(null, s);
-		operator.push(s);
-		operand.push(igd);
+		s = JOptionPane.showInputDialog(null, "Bitte gib deinen vollständig geklammerten algebraischer Ausdruck ein:");
+//		operator.push(s);
+//		operand.push(igd);
+		while(s.length() > 0) {
+			String snippet = s.substring(0, 1);
+			s = s.substring(1);
+			if (snippet.matches("[)]")) {
+				
+				int partResult = 0;
+				char ch = operator.pop().charAt(0);
+				switch (ch) {
+					case '+': 	
+						partResult = operand.pop().intValue() + operand.pop().intValue();
+						break;
+					case '*': 	
+						partResult = operand.pop().intValue() * operand.pop().intValue();	
+						break;
+					case '-': 	
+						partResult = 0 - operand.pop().intValue() + operand.pop().intValue();
+						break;
+					case '/': 	
+						partResult = operand.pop().intValue();
+						partResult = operand.pop().intValue() / partResult;
+				}
+				
+				
+				operand.push(new Integer(partResult));
+			}
+			else if (snippet.matches("[+-/*]")) {
+				operator.push(snippet);
+			}
+			else if (snippet.matches("[0-9]")) {
+				operand.push(new Integer(snippet));
+			}
+			else {
+				if (!snippet.equals("(")) {
+					throw new IllegalArgumentException("Kein gültiger Ausdruck");
+				}
+				
+			}
+		}
+		System.out.println(operand.pop().intValue());
 	}
 	
 	
-	public void rechne() {
+	public void rechne(String s) {
 		
 	}
 }
