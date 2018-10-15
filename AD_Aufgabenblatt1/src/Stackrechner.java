4c015c37b926325cb7e5263de4ed2b4b2e4d5e22
acj273 <acj273@52-LUI.mailcluster.haw-hamburg.de>
2018-10-15 14:20:51 +0200

    fixed mehrere digits

--- AD_Aufgabenblatt1/src/Stackrechner.java
+++ AD_Aufgabenblatt1/src/Stackrechner.java
@@ -2,23 +2,15 @@ import javax.swing.JOptionPane;
 
 public class Stackrechner
 {
-//	private static Stack<T> _operator;
-//	private static Stack<T> _operand;
-	
-	
-	
 	public static void main(String[] args) {
 		Stack<String> operator = new Stack<String>();
 		Stack<Integer> operand = new Stack<Integer>();
 		String s = "";
 		s = JOptionPane.showInputDialog(null, "Bitte gib deinen vollständig geklammerten algebraischer Ausdruck ein:");
-//		operator.push(s);
-//		operand.push(igd);
 		while(s.length() > 0) {
 			String snippet = s.substring(0, 1);
 			s = s.substring(1);
 			if (snippet.matches("[)]")) {
-				
 				int partResult = 0;
 				char ch = operator.pop().charAt(0);
 				switch (ch) {
@@ -35,28 +27,34 @@ public class Stackrechner
 						partResult = operand.pop().intValue();
 						partResult = operand.pop().intValue() / partResult;
 				}
-				
-				
 				operand.push(new Integer(partResult));
 			}
 			else if (snippet.matches("[+-/*]")) {
 				operator.push(snippet);
 			}
 			else if (snippet.matches("[0-9]")) {
+				snippet = snippet + getDigits(s);
+				s = s.substring(getDigits(s).length());
 				operand.push(new Integer(snippet));
 			}
 			else {
 				if (!snippet.equals("(")) {
 					throw new IllegalArgumentException("Kein gültiger Ausdruck");
 				}
-				
 			}
 		}
 		System.out.println(operand.pop().intValue());
 	}
 	
-	
-	public void rechne(String s) {
-		
+	private static String getDigits(String s) {
+		String digits = s.substring(0, 1);
+		String result ="";
+		int i = 1;
+		while (digits.matches("[0-9]+")) {
+			result = digits;
+			++i;
+			digits = s.substring(0, i);
+		}
+		return result;
 	}
 }
